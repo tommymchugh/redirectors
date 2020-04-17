8c0b504792a8db750eb59379d7ecd5a387e8f001
Tommy McHugh <tombmch@gmail.com>
2020-04-17 13:06:11 -0500

    adding relevant new urls

--- tommy/redirect.js
+++ tommy/redirect.js
@@ -8,6 +8,33 @@ const paths = {
   "github": {
     "url": "https://github.com/tommymchugh"
   },
+  "mail": {
+    "url": "https://mail.google.com/a/u.northwestern.edu"
+  },
+  "calendar": {
+    "url": "https://calendar.google.com"
+  },
+  "itl": {
+    "url": "https://github.com/inclusivetechnu/itl",
+    "issues": {
+      "url": "https://github.com/inclusivetechnu/itl/issues"
+    },
+    "pr": {
+      "url": "https://github.com/inclusivetechnu/itl/pulls"
+    },
+    "commits": {
+      "url": "https://github.com/InclusiveTechNU/itl/commits/master"
+    },
+    "status": {
+      "url": "https://travis-ci.com/InclusiveTechNU/itl"
+    }
+  },
+  "dtr": {
+    "url": "https://calendar.google.com",
+    "studio": {
+      "url": "https://northwestern.zoom.us/j/108696191?pwd=SGNkaEp5UnlBc1MzbHVxU1NOL1cwdz09"
+    }
+  },
 };
 
 const redirectHandler = "tommy";
@@ -15,7 +42,11 @@ const httpHandler = `://${redirectHandler}/`;
 
 const performRequest = (path) => {
   const components = path.split("/");
-  return paths[components[0]].url;
+  let urlObject = paths;
+  for (let i = 0; i < components.length; i++) {
+    urlObject = urlObject[components[i]];
+  }
+  return urlObject.url;
 };
 
 chrome.webRequest.onBeforeRequest.addListener((details) => {
